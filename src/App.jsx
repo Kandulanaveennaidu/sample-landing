302a700be2e479fe078346f7a93798324069ab3c
kandula Naveen <kandulanaveennaidu017@gmail.com>
2023-07-12 17:19:11 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { FiDownload, FiUser } from "react-icons/fi";
 import "./App.css";
 import health from "./assets/954528 (1)@2x.png";
 import hero from "./assets/Untitled-1@2x.png";
@@ -11,19 +12,27 @@ import dental from "./assets/1881195@2x.png";
 import muscle from "./assets/1881195@2x.png";
 import liver from "./assets/1881195@2x.png";
 import brain from "./assets/1713596@2x.png";
+import header from "./assets/MEDDTODAY@2x.png";
+
 function App() {
   return (
     <div className="App">
       <header>
         <div className="header-section">
           <div className="header-logo">
-            <img src="logo.png" alt="Logo" />
+            <img src={header} alt="Logo" />
           </div>
           <div className="header-buttons">
             <button className="help-button">Need Help?</button>
-            <button className="download-app-button">Download App</button>
+            <button className="download-app-button">
+              <FiDownload />
+              Download App
+            </button>
             <button className="cart-button">Cart</button>
-            <button className="profile-button">Profile</button>
+            <button className="profile-button">
+              <FiUser />
+              Profile
+            </button>
           </div>
         </div>
         <div className="header-section">
@@ -34,16 +43,17 @@ function App() {
             <button className="search-button">Search</button>
           </div>
         </div>
+        <div className="header-section">
+          <h2 className="header-categories">
+            COVID Essentials | Eye Wear | Ayush | Ayurvedic | Homeopathy |
+            Fitness | Mom & Baby | Devices | Surgicals | Sexual Wellness |
+            Treatments
+          </h2>
+        </div>
       </header>
+
       <section className="hero-section">
-        <motion.img
-          src={hero}
-          alt="Hero"
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        />
-        <div className="hero-content">
+        <div className="hero-content-left">
           <motion.h3
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
@@ -51,15 +61,26 @@ function App() {
           >
             FLAT 25% OFF on MEDICINE order
           </motion.h3>
-          <motion.button
-            className="order-now-button"
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-          >
-            Order Now
-          </motion.button>
+
+          <p className="hero-description">
+            1. Upload valid prescription
+            <br />
+            2. Receive a confirmation call
+            <br />
+            3. Delivery at your doorstep
+          </p>
+        </div>
+        <div className="hero-image-right">
+          <motion.img
+            src={hero}
+            alt="Hero"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          />
         </div>
       </section>
+
       <footer>
         <div className="footer-section">
           <img src={health} alt="Heart Care" />
